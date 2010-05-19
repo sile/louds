75fee75ef612e3088e9ec7dd2c7718eb15003675
t_ohta <phjgt308@ybb.ne.jp>
2010-05-20 02:44:25 +0900

    保存

---
+++ louds.asd
@@ -0,0 +1,10 @@
+(in-package :asdf)
+
+(defsystem louds
+  :name "louds"
+  :version "0.0.1"
+  :author ""
+  :description ""
+  
+  :serial t
+  :components ((:file "louds")))
---
+++ louds.lisp
@@ -0,0 +1,61 @@
+(defpackage louds
+  (:use :common-lisp))
+(in-package :louds)
+
+	   
+(defun select0 (bits i)
+  "BITSの中のi番目の0の位置を返す。iが0以下、もしくはi番目の0がBITSに存在しない場合は-1を返す。"
+  (loop FOR pos FROM 0
+	FOR bit ACROSS bits
+    WHEN (= 0 bit)
+    DO (when (zerop (decf i))
+	 (return pos))
+    FINALLY (return -1)))
+
+; ノードNのビット配列内での位置
+(defun select1 (bits i)
+  "BITSの中のi番目の1の位置を返す。iが0以下、もしくはi番目の1がBITSに存在しない場合は-1を返す。"
+  (loop FOR pos FROM 0
+	FOR bit ACROSS bits
+    WHEN (= 1 bit)
+    DO (when (zerop (decf i))
+	 (return pos))
+    FINALLY (return -1)))
+	
+; 位置Xにあるビットがどのノードに対応するか
+(defun rank0 (bits x)
+  "BITS内で、位置Xよりも左(Xも含む)にある0ビットの数を返す。"
+  (loop FOR i FROM x DOWNTO 0
+	FOR bit = (aref bits i)
+    WHEN (= 0 bit)
+    SUM 1))
+
+(defun rank1 (bits x)
+  "BITS内で、位置Xよりも左(Xも含む)にある1ビットの数を返す。"
+  (loop FOR i FROM x DOWNTO 0
+	FOR bit = (aref bits i)
+    WHEN (= 1 bit)
+    SUM 1))
+
+;; lbs = louds bit string
+(use-package :common-utils)
+
+(defvar *tree* 
+  '(:a (:b)
+       (:c (:f) 
+           (:g (:i))) 
+       (:d) 
+       (:e (:h))))
+;; (:a (:b) (:c (:f) (:g (:i))) (:d) (:e (:h)))
+(defun tree-to-lbs (tree)
+  (coerce
+   (loop WITH  tree-que = `((:super-root ,tree))
+         WHILE tree-que
+     APPEND
+     (destructuring-bind ((node-name . children) . rest-trees) tree-que
+       (declare (ignore node-name))
+       (setf tree-que (append rest-trees children))
+       (append (loop REPEAT (length children) COLLECT 1) 
+               '(0))))
+   'bit-vector))
+                 
