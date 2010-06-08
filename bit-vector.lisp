01e41a296a7b46d37a4753ac1d6c8c77f28e9bc7
t_ohta <phjgt308@ybb.ne.jp>
2010-06-09 06:12:05 +0900

    bit-vector: 経過保存(整理中)

--- bit-vector.lisp
+++ bit-vector.lisp
@@ -1,141 +1,173 @@
 (in-package :louds)
 
-(defconstant +BLOCK-SIZE+ 64)
-(defconstant +SELECT-INDEX-INTERVAL+ 32)
-
 (defvar *sample-bits*
   (coerce (loop REPEAT 1000 COLLECT (if (oddp (random 5)) 1 0)) 'bit-vector))
 
-(deftype index () '(mod #.array-total-size-limit))
+(deftype uint32 () '(unsigned-byte 32))
+(deftype uint16 () '(unsigned-byte 16))
+(deftype uint8  () '(unsigned-byte 8))
+(deftype array-index () '(mod #.array-total-size-limit))
 
-(defstruct bv
-  ;; for select/rank
-  (blocks          #() :type (simple-array (unsigned-byte 32)))
-  (0bit-acc-counts #() :type (simple-array (unsigned-byte 32)))
-  
-  ;; for select
-  (1bit-counts     #() :type (simple-array (unsigned-byte 8)))
-  (sel-indices     #() :type (simple-array (unsigned-byte 16)))
+(defconstant +BLOCK-SIZE+ 64)
+(defconstant +WORD-SIZE+  32)
+(defconstant +SELECT-INDEX-INTERVAL+ 32)
 
-  (1bit-cnt-until-last-selidx #() :type (simple-array (unsigned-byte 8)))
-  
-  ;; for rank
-  (all-0bit-flags  #() :type (simple-array (unsigned-byte 32)))
-  (a0f-rank-aux    #() :type (simple-array (unsigned-byte 32))))  ;; -> rank-indices
+(defstruct bitvector
+  (blocks                                   t :type (simple-array uint32))
+  (block-precede-0bit-count                 t :type (simple-array uint32))
+  (block-1bit-count                         t :type (simple-array uint8))
+  (block-1bit-count-until-last-select-index t :type (simple-array uint8))
+  (select-indices                           t :type (simple-array uint16))
+  (src-block-all-0bit-flag                  t :type (simple-array uint32))
+  (SBC0F-rank-indices                       t :type (simple-array uint32)))
 
-(defun bits-to-num (bits start end)
-  (loop FOR i FROM start BELOW (min end (length bits))
-	FOR b = (bit bits i)
-    SUM (ash b (- i start))))
+(defun bits-to-num (bits &optional (start 0) (end (length bits)))
+  (loop FOR i      FROM start BELOW (min end (length bits))
+	FOR offset FROM 0
+	SUM (ash (bit bits i) offset)))
 
-(defun to-u32-blocks (tmp-blocks)
-  (coerce
-   (loop FOR bs ACROSS tmp-blocks
-	 APPEND
-	 (loop FOR i FROM 0 BELOW +BLOCK-SIZE+ BY 32
-	       COLLECT (bits-to-num bs i (+ i 32))))
-   '(vector (unsigned-byte 32))))
+(defun divide-bit-string (bit-string block-size &aux (len (length bit-string)))
+  (loop FOR start FROM 0 BELOW len BY block-size
+	COLLECT (subseq bit-string start (min (+ start block-size) len)) INTO bits-list
+	FINALLY (return (coerce bits-list 'vector))))
 
-(defun count-acc-0bit (tmp-blocks)
-  (coerce
-   (loop FOR bs ACROSS tmp-blocks
-	 FOR 0bit-cnt = (count 0 bs)
-	 AND acc = 0 THEN (+ acc 0bit-cnt)
-     UNLESS (= +BLOCK-SIZE+ 0bit-cnt)
-     COLLECT acc)
-   '(vector (unsigned-byte 32))))
+(defun make-bit-blocks (bit-string)
+  (divide-bit-string bit-string +BLOCK-SIZE+))
 
-(defun count-1bit-per-block (tmp-blocks-rem-all0)
-  (coerce
-   (loop FOR bs ACROSS tmp-blocks-rem-all0
-     COLLECT (count 1 bs))
-   '(vector (unsigned-byte 8))))
+(defun make-blocks (bit-blocks)
+  (loop FOR bits ACROSS bit-blocks
+    APPEND `(,(bits-to-num bits 0 +WORD-SIZE+)
+	     ,(bits-to-num bits +WORD-SIZE+ +BLOCK-SIZE+)) INTO block-list
+    FINALLY (return (coerce block-list '(vector uint32)))))
 
-(defun compress-sel-indices (list &optional acc (base 0) (i 0))
-  (if (endp list)
-      (coerce (nreverse acc) '(vector (unsigned-byte 16)))
-    (destructuring-bind (n . rest) list
-      (if (zerop i)
-	  (compress-sel-indices rest `(,(ldb (byte 16 0) n) ,(ldb (byte 16 16) n) . ,acc) n 7)
-	(compress-sel-indices rest `(,(- n base) . ,acc) base (1- i))))))
+(defun make-block-precede-0bit-count (bit-blocks)
+  (loop FOR bits ACROSS bit-blocks
+	FOR count = (count 0 bits)
+	AND total = 0 THEN (+ total count)
+    UNLESS (= +BLOCK-SIZE+ count)
+    COLLECT total INTO 0bit-counts
+    FINALLY (return (coerce 0bit-counts '(vector uint32)))))
 
-(defun calc-sel-indices (tmp-blocks-per-all0)
-  (compress-sel-indices
-   (cons 0 ;; XXX: きれいじゃない
-   (loop WITH nth = 0
-	 WITH i   = 0
-	 FOR bs ACROSS tmp-blocks-per-all0
-     APPEND
-     (loop FOR b ACROSS bs
-       WHEN (and (incf i)
-		 (= b 1)
-		 (incf nth)
-		 (zerop (mod nth +SELECT-INDEX-INTERVAL+)))
-       COLLECT i)))))
+(defun make-block-1bit-count (bit-blocks)
+  (loop FOR bits ACROSS bit-blocks 
+    COLLECT (count 1 bits) INTO 1bit-counts
+    FINALLY (return (coerce 1bit-counts '(vector uint8)))))
 
-(defun calc-all-0bit-flags (tmp-blocks)
-  (coerce (loop FOR bs ACROSS tmp-blocks
-		COLLECT (if (zerop (count 1 bs)) 1 0))
-	  'bit-vector))
+(defun make-select-indices-list (bit-blocks)
+  (loop WITH index = 0
+	WITH nth   = 0
+	FOR bits ACROSS bit-blocks
+    APPEND (loop FOR b ACROSS bits
+             WHEN (and (incf index)
+		       (= b 1)
+		       (incf nth)
+		       (zerop (mod nth +SELECT-INDEX-INTERVAL+)))
+	     COLLECT index)
+      INTO indices
+    FINALLY (return (cons 0 indices))))
 
-(defun to-u32-flags (bits)
-  (coerce
-   (loop FOR i FROM 0 BELOW (length bits) BY 32
-	 COLLECT (bits-to-num bits i (+ i 32)))
-   '(vector (unsigned-byte 32))))
+(defun make-select-indices (select-indices-list &optional encoded-indices (base 0) (i 0))
+  (flet ((low  (n) (ldb (byte 16 0)  n))
+	 (high (n) (ldb (byte 16 16) n)))
+    (if (endp select-indices-list)
+	(coerce (nreverse encoded-indices) '(vector uint16))
+      (destructuring-bind (idx . rest) select-indices-list
+        (if (zerop i)
+	    (make-select-indices rest `(,(high idx) ,(low idx) ,@encoded-indices) idx 7)
+	  (make-select-indices rest `(,(- idx base) ,@encoded-indices) base (1- i)))))))
 
-(defun calc-aux (flags)
-  (coerce
-   (loop WITH nth = 0
-	 FOR u32 ACROSS flags
-     COLLECT (prog1 nth
-	       (incf nth (logcount u32))))
-   '(vector (unsigned-byte 32))))
-
-(defun calc0 (tmp-blocks-per-all0)
-  (coerce 
-   ;; TODO: cons 0 ?
-   (loop WITH nth = 0
-	 WITH i   = 0
-	 FOR bs ACROSS tmp-blocks-per-all0
-     APPEND
-     (loop FOR b ACROSS bs
-       WHEN (and (incf i)
-		 (= b 1)
-		 (incf nth)
-		 (zerop (mod nth +SELECT-INDEX-INTERVAL+)))
-       COLLECT i))
-   '(vector (unsigned-byte 32))))
-
-;; TODO: declaim: (defun rank1-block (pos low high)K(defun select1-block (nth block-low block-high)
-(defun calc1 (sel-indices blocks block-size)
-  (let ((ary (make-array block-size :element-type '(unsigned-byte 8) :initial-element 0)))
-    (loop FOR idx ACROSS sel-indices 
+(defun make-block-1bit-count-until-last-select-index (select-indices-list bit-blocks)
+  (let ((ary (make-array (length bit-blocks) :element-type 'uint8 :initial-element 0)))
+    (loop FOR idx IN select-indices-list
 	  FOR block-num = (floor idx +BLOCK-SIZE+)
+	  FOR bits      = (aref bit-blocks block-num)
       DO (setf (aref ary block-num)
-	       (rank1-block (mod idx +BLOCK-SIZE+)
-			    (aref blocks (+ 0 (* 2 block-num)))
-			    (aref blocks (+ 1 (* 2 block-num))))))
+	       (count 1 bits :end (mod idx +BLOCK-SIZE+))))
     ary))
 
-(defun build-bv (bits &aux (bits-len (length bits)))
-  (let* ((tmp-blocks (coerce 
-		      (loop FOR i FROM 0 BELOW bits-len BY +BLOCK-SIZE+
-			    COLLECT (subseq bits i (min (+ i +BLOCK-SIZE+) bits-len)))
-		      'vector))
-	 (tmp-blocks-rem-all0 (remove-if (lambda (bs) (every #'zerop bs)) tmp-blocks)))
-    (make-bv
-     :blocks #2=(to-u32-blocks tmp-blocks-rem-all0)
-     :0bit-acc-counts (count-acc-0bit tmp-blocks)
-     :1bit-counts (count-1bit-per-block tmp-blocks-rem-all0)
-     :sel-indices (calc-sel-indices tmp-blocks-rem-all0)
-     :1bit-cnt-until-last-selidx (calc1 (calc0 tmp-blocks-rem-all0) #2# (length tmp-blocks-rem-all0))
-     :all-0bit-flags #1=(to-u32-flags (calc-all-0bit-flags tmp-blocks))
-     :a0f-rank-aux (calc-aux #1#))))
+(defun make-src-block-all-0bit-flag (bit-blocks)
+  (let ((flag-bits
+	 (loop FOR bits ACROSS bit-blocks
+	       COLLECT (if (zerop (count 1 bits)) 1 0) INTO flag-bits
+	       FINALLY (return (coerce flag-bits 'bit-vector)))))
+    (map '(vector uint32) #'bits-to-num (divide-bit-string flag-bits +WORD-SIZE+))))
+  
+(defun make-SBC0F-rank-indices (src-block-all-0bit-flag)
+  (loop WITH nth = 0
+	FOR uint32 ACROSS src-block-all-0bit-flag
+    COLLECT (prog1 nth (incf nth (logcount uint32))) INTO rank-indices
+    FINALLY (return (coerce rank-indices '(vector uint32)))))
 
+(defun build-bitvector (bit-string)
+  (let* ((bit-blocks (make-bit-blocks bit-string))
+	 (bit-blocks-no-all0 (remove-if (lambda (bits) (every #'zerop bits)) bit-blocks))
+	 (blocks     (make-blocks bit-blocks-no-all0))
+	 (select-indices-list (make-select-indices-list bit-blocks-no-all0))
+	 (src-block-all-0bit-flag (make-src-block-all-0bit-flag bit-blocks)))
+    (make-bitvector
+     :blocks                   blocks
+     :block-precede-0bit-count (make-block-precede-0bit-count bit-blocks)
+     :block-1bit-count         (make-block-1bit-count bit-blocks-no-all0)
+     :block-1bit-count-until-last-select-index
+       (make-block-1bit-count-until-last-select-index select-indices-list bit-blocks-no-all0)
+     :select-indices           (make-select-indices select-indices-list)
+     :src-block-all-0bit-flag  src-block-all-0bit-flag
+     :SBC0F-rank-indices       (make-SBC0F-rank-indices src-block-all-0bit-flag))))
 
+;;;;;;;;;;;
+;;;; select
+(defun get-base-block (nth bitvector)
+  )
+
+(defun goto-target-block (start-block-num precede-1bit-count bitvector)
+  (with-slots (block-1bit-count) bitvector
+    (loop FOR block-num FROM start-block-num
+	  FOR 1cnt     = (aref block-1bit-count block-num)
+	  AND pre-1cnt = precede-1bit-count THEN (+ pre-1cnt 1cnt)
+      WHILE (> nth (+ pre-1cnt 1cnt))
+      FINALLY (return (values block-num pre-1cnt)))))
+
+(defun select1 (nth bitvector)
+  (multiple-value-bind (base-block-num 1bit-count)
+		       (get-base-block nth bitvector)
+    (multiple-value-bind (block-num 1bit-count) 
+			 (goto-target-block base-block-num 1bit-count bitvector)
+      (+ 1bit-count                                ; 1bit count
+	 (aref block-precede-0bit-count block-num) ; 0bit count
+	 (block-select1 (- nth 1bit-count) block-num bitvector)))))
+
+#+IGNORE
+(defun select1 (nth bv)
+  (with-slots (blocks 1bit-counts 0bit-acc-counts) bv
+    (multiple-value-bind (div #|rem|#) (floor nth 32)
+      (let* ((base-pos (get-base-pos div bv))
+	     (base-block-num (* 2 (floor base-pos +BLOCK-SIZE+)))
+	     (base-block-low  (aref blocks (+ 0 base-block-num)))
+	     (base-block-high (aref blocks (+ 1 base-block-num)))
+	     (base-rank1 (get-acc-1bit-count (* div 32)
+					     base-pos
+					     base-block-low base-block-high
+					     bv 
+					     (/ base-block-num 2) 
+					     )))
+	;;(print `(,base-pos ,base-block-num ,base-block-low ,base-block-high ,base-rank1))
+	(multiple-value-bind (block-num rank1)
+	  (loop FOR block FROM (floor base-block-num 2)
+		AND rank1 = base-rank1 THEN (+ rank1 (aref 1bit-counts block))
+	    WHILE (> nth (+ rank1 (aref 1bit-counts block)))
+	    FINALLY (return (values block rank1)))
+	  ;;(print `(,rank1 ,(aref 0bit-acc-counts block-num) ,block-num))
+	  (+ rank1
+	     (aref 0bit-acc-counts block-num)
+	     (select1-block (- nth rank1)
+			    (aref blocks (+ 0 (* 2 block-num)))
+			    (aref blocks (+ 1 (* 2 block-num))))))))))
+
+#|
+;;;;;;;;;;;
+;;;; select
 (defun get-base-pos (div bv)
-  (declare (index div)
+  (declare (array-index div)
 	   (bv bv)
 	   #.*fastest*)
   (with-slots (sel-indices) bv
@@ -234,6 +266,8 @@
 			    (aref blocks (+ 0 (* 2 block-num)))
 			    (aref blocks (+ 1 (* 2 block-num))))))))))
 
+;;;;;;;;;
+;;;; rank
 (defun rank1 (pos bv)
   (with-slots (0bit-acc-counts blocks) bv
     (let* ((adj-pos (- pos (* (deleted-block-num pos bv) +BLOCK-SIZE+)))
@@ -271,3 +305,4 @@
        (* (length sel-indices) 16)
        (* (length all-0bit-flags) 32)
        (* (length a0f-rank-aux) 32))))
+|#
